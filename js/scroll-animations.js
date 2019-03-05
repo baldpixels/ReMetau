21393a096c2e2022338a82d5574157ae3672fd7d
baldpixels <baldpixels@gmail.com>
2019-03-04 16:03:38 -0800

    animations added

--- js/scroll-animations.js
+++ js/scroll-animations.js
@@ -26,15 +26,57 @@ $( document ).ready( function() {
     let precision = 10000;
     scrollPercent = Math.round( precision * $(window).scrollTop() / ($(document).height() - $(window).height()) ) / precision;
 
-    console.log(wrapper.height());
-    console.log($(window).height());
-    console.log($(document).height());
-
     riseHeight = initialHeight + scrollPercent * $(window).height();
 
-    //console.log(riseHeight);
-
     ocean.css('height', riseHeight);
+  }
+
+  function animateIn(element) {
+    $($(element)).css('visibility', 'visible');
+
+    if($(element).hasClass('title') || $(element).hasClass('textCenter')) {
+      $(element).addClass('animated fadeInDown');
+    }
+    else if($(element).hasClass('subtitle')) {
+      $(element).addClass('animated fadeInUp');
+    }
+    else if($(element).hasClass('textLeft') || $(element).hasClass('mediaLeft') || $(element).hasClass('captionLeft')) {
+      $(element).addClass('animated fadeInLeft');
+    }
+    else if($(element).hasClass('textRight') || $(element).hasClass('mediaRight') || $(element).hasClass('captionRight')) {
+      $(element).addClass('animated fadeInRight');
+    }
+    else if($(element).hasClass('icon')) {
+      $(element).addClass('animated zoomIn');
+    }
+    else {
+      $(element).addClass('animated fadeInUp');
+    }
+  }
+
+  // these classes should animate in on scroll...
+  var classes = ['textLeft', 'textRight', 'textCenter', 'title', 'subtitle', 'icon', 'mediaLeft', 'mediaRight', 'captionLeft', 'captionRight'];
+  var ids = [];
+
+  for (var i=0; i<classes.length; i++) {
+    $('.' + classes[i]).each(function(j, obj) {
+      // give every instance of classes[i] an id of form '#classname1', ''#classname2', etc.
+      $(obj).attr('id', classes[i] + j);
+      ids.push(classes[i] + j);
+    });
+  }
+
+  // now attach Waypoint object to each id
+  for (var i=0; i<ids.length; i++) {
+    var waypoint = new Waypoint({
+      element: document.getElementById(ids[i]),
+      handler: function(direction) {
+        if(direction == 'down') {
+          animateIn(this.element);
+        }
+      },
+      offset: '100%'
+    });
   }
 
 // EVENT LISTENERS
